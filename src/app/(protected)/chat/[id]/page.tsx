5ba3ddadbfcde34096abde8fbeaa2d972204624c
Richard Kovacs <richardkovacs.it@gmail.com>
2025-06-11 19:07:45 +0200

    feat(sharing): disable when jwt secret is empty

--- src/app/(protected)/chat/[id]/page.tsx
+++ src/app/(protected)/chat/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ShareConversationButton from "@/components/chat/share-conversation-button";
 import { MessagesList } from "@/components/message-list";
 import MessagesScrollArea from "@/components/messages-scroll-area";
+import conf from "@/lib/config";
 import { getConversation } from "@/lib/dao/conversations";
 import { getMessages } from "@/lib/dao/messages";
 import { getUserIdFromSession } from "@/lib/dao/users";
@@ -15,6 +16,8 @@ export default async function ChatPage({
 
   const { id } = await params;
 
+  const jwtConfigured = conf.jwtSecret !== "";
+
   const conversationData = getConversation(id);
   const messagesData = getMessages(id, 1, 10);
   const [conversation, messages] = await Promise.all([conversationData, messagesData]);
@@ -22,10 +25,12 @@ export default async function ChatPage({
   const formattedMessages = processMessages(messages.messages);
   return (
     <>
-      <ShareConversationButton
-        conversationId={id}
-        className="absolute top-4 right-4 md:top-2 md:right-2 z-10"
-      />
+      {jwtConfigured && (
+        <ShareConversationButton
+          conversationId={id}
+          className="absolute top-4 right-4 md:top-2 md:right-2 z-10"
+        />
+      )}
       <MessagesScrollArea className="relative h-[calc(100svh-114px)] md:h-[calc(100svh-142px)]">
         <div className="absolute top-0 left-0 right-0 h-6 bg-gradient-to-b from-background to-transparent pointer-events-none z-10 rounded-t-xl" />
         <MessagesList
