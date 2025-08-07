7ed3bc214d638ac548c88424c4315dc5d610f353
Richard Kovacs <richardkovacs.it@gmail.com>
2025-08-07 22:30:48 +0200

    fix(conversation): fix initial load

--- src/components/v4/v4-id-page.tsx
+++ src/components/v4/v4-id-page.tsx
@@ -27,11 +27,7 @@ export default function V4IdPage({
       )}
       <MessagesScrollArea className="relative h-[calc(100svh-170px)] md:h-[calc(100svh-198px)]">
         <div className="absolute top-0 left-0 right-0 h-6 bg-gradient-to-b from-background to-transparent pointer-events-none z-10" />
-        <MessagesList
-          id={id as string}
-          initialConversation={[]}
-          initialMessages={{ messages: [], hasMore: false }}
-        />
+        <MessagesList id={id as string} />
       </MessagesScrollArea>
     </ViewTransitionWrapper>
   );
