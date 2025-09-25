6c52b33c45b44511be1c543b07ee563a88ffed90
Richard Kovacs <richardkovacs.it@gmail.com>
2025-09-25 21:24:24 +0200

    refactor(search-menu): change select to dropdown radio group

--- src/components/input-form/search-menu.tsx
+++ src/components/input-form/search-menu.tsx
@@ -1,20 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useChatStore } from "@/stores/chat-store";
 import { useModelStore } from "@/stores/model-store";
+import { Globe } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function SearchMenu() {
   const model = useModelStore((state) => state.model);
+  const searchSupport = model.features?.some((feature) => feature.name === "Search");
   const isSonar = model.id === "sonar" || model.id === "sonar-pro";
   const [search, setSearch] = useState(isSonar ? "web" : "none");
   const { setBrowse } = useChatStore();
@@ -22,7 +25,7 @@ export default function SearchMenu() {
   // Update search state when model changes
   useEffect(() => {
     setSearch(isSonar ? "web" : "none");
-  }, [isSonar]);
+  }, [isSonar, searchSupport]);
 
   function handleChange(value: string) {
     setSearch(value);
@@ -30,17 +33,20 @@ export default function SearchMenu() {
   }
 
   return (
-    <Select value={search} onValueChange={handleChange}>
-      <SelectTrigger disabled={isSonar}>
-        <SelectValue placeholder="Search" />
-      </SelectTrigger>
-      <SelectContent onCloseAutoFocus={(e) => e.preventDefault()}>
-        <SelectGroup>
-          <SelectLabel>Search type</SelectLabel>
-          <SelectItem value="none">None</SelectItem>
-          <SelectItem value="web">Web</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild disabled={isSonar || !searchSupport}>
+        <Button variant="outline" size="icon" className="shrink-0">
+          <Globe size={16} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Search type</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={search} onValueChange={handleChange}>
+          <DropdownMenuRadioItem value="none">None</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="web">Web</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
