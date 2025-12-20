605c83953c7dec1904ea0d4c3623cd7e7ffd07e0
Richard Kovacs <richardkovacs.it@gmail.com>
2025-12-20 01:06:28 +0100

    style(kbd): adjust radius

--- src/components/ui/kbd.tsx
+++ src/components/ui/kbd.tsx
@@ -6,7 +6,7 @@ function Kbd({ className, ...props }: ComponentProps<"kbd">) {
     <kbd
       data-slot="kbd"
       className={cn(
-        "pointer-events-none inline-flex h-5 w-fit min-w-5 select-none items-center justify-center gap-1 rounded-sm bg-muted px-1 font-medium font-sans text-muted-foreground text-xs",
+        "pointer-events-none inline-flex h-5 w-fit min-w-5 select-none items-center justify-center gap-1 rounded-lg bg-muted px-1 font-medium font-sans text-muted-foreground text-xs",
         "[&_svg:not([class*='size-'])]:size-3",
         "in-data-[slot=tooltip-content]:bg-background/20 in-data-[slot=tooltip-content]:text-background dark:in-data-[slot=tooltip-content]:bg-background/10",
         className
